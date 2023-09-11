9af2cdc0868e9ea802d62e390ead7f10fba06fb8
Chris1998c <contechristian.98@gmail.com>
2023-09-11 15:26:25 +0200

    ciao

---
+++ exercise.js
@@ -0,0 +1,13 @@
+let num1 = 10;
+let num2 = 20;
+console.log(`Before swap:`,num1, num2); // Before swap: 10 20
+
+
+
+
+// variabile cambio
+
+[num1, num2] = [num2, num1];
+
+
+console.log(`cambiata:`,num1, num2);
